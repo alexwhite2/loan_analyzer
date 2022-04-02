b0625758c11777e07a3f7d50fc6bb11085220e07
alexwhite2 <101836699+alexwhite2@users.noreply.github.com>
2022-04-02 12:35:07 -0700

    Add files via upload
    Updated folder GitHub_Upload for part 2 of project

--- GitHub_Upload/loan_analyzer.py
+++ GitHub_Upload/loan_analyzer.py
@@ -15,25 +15,29 @@ First, let's start with some calculations on a list of prices for 5 loans.
 loan_costs = [500, 600, 200, 1000, 450]
 number_of_loans = len(loan_costs)
 sum_of_loans = sum (loan_costs)
+average_loan_price = sum_of_loans / number_of_loans
 
-print (f"The total number of loans is:",number_of_loans)
-print (f"The total sum of all loans is: $",sum_of_loans)
-print (f"The average price of a loan is: $",sum(loan_costs))
 
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
 # YOUR CODE HERE!
+number_of_loans = len(loan_costs)
+print (f"The total number of loans is",number_of_loans)
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
+sum_of_loans = sum (loan_costs)
+print (f"The total sum of all loans is $",sum_of_loans)
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
+average_loan_price = sum_of_loans / number_of_loans
+print (f"The average price of a loan is $",average_loan_price)
 
 """Part 2: Analyze Loan Data.
 
